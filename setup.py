0effd41a352acb8c3245215773d9ed3c39d0b4e7
Arian Askari <diyardownload@gmail.com>
2024-08-18 17:20:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,8 +7,11 @@ setup(
     author="Arian Askari, Roxana Petcu",
     packages=find_packages(),
     install_requires=[
-        "transformers",  # You can add other dependencies as needed
+        "bitsandbytes==0.43.3",
+        "transformers @ git+https://github.com/huggingface/transformers.git@52cb4034ada381fe1ffe8d428a1076e5411a8026",
+        "peft @ git+https://github.com/huggingface/peft.git@e8ba7de5732e050a8f061cde555c02ab575f7529",
+        "accelerate @ git+https://github.com/huggingface/accelerate.git@589fddd317f008e704073c133bc2cb8958f287e6",
         "tqdm",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.10, <3.13",
 )
